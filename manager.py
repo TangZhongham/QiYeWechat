26e0bf123c42e7020686d6115fb602851258476b
Gary Tang <13122260573@163.com>
2019-01-27 18:40:37 +0800

    回调接口开发成功

--- manager.py
+++ manager.py
@@ -1,19 +1,27 @@
 from flask import Flask
-from flask import request
+from flask import request, abort
 from utils.get_access_token import get_access_token
 import requests
 import json
 import sys
 from configparser import ConfigParser
 
+from wechatpy.enterprise.crypto import WeChatCrypto
+from wechatpy.exceptions import InvalidSignatureException
+from wechatpy.enterprise.exceptions import InvalidCorpIdException
+from wechatpy.enterprise import parse_message, create_reply
 
-from .utils.WXBizMsgCrypt import WXBizMsgCrypt
+
+TOKEN = 'lw2blf8'
+EncodingAESKey = '6ObhL0l5EmsGUYDaeJgKyxqAH7ixraGEJFmO9FJieWP'
+APPID = '1000002'
+CorpId = 'ww8059a2127f62bc2a'
 
 
 app = Flask(__name__)
 
 
-@app.route('/<msg_text>')
+@app.route('/msg/<msg_text>')
 def index(msg_text):
     ac_token = get_access_token()
     if ac_token:
@@ -34,51 +42,47 @@ def index(msg_text):
         print(a)
         if a:
             return "<h1> success </h1>\n%s" % a
+        else:
+            return "failed"
 
 
-@app.route('/auto-reply')
-def auto_reply(msg):
-    wxcpt = WXBizMsgCrypt(self.sToken, self.sEncodingAESKey, self.sCorpID)
-    # 获取url验证时微信发送的相关参数
-    sVerifyMsgSig = request.args.get('msg_signature')
-    sVerifyTimeStamp = request.args.get('timestamp')
-    sVerifyNonce = request.args.get('nonce')
-    sVerifyEchoStr = request.args.get('echostr')
+@app.route('/auto', methods=['GET', 'POST'])
+def auto_reply():
+    signature = request.args.get('msg_signature', '')
+    timestamp = request.args.get('timestamp', '')
+    nonce = request.args.get('nonce', '')
 
-    # 验证url
+    crypto = WeChatCrypto(TOKEN, EncodingAESKey, CorpId)
     if request.method == 'GET':
-        ret, sEchoStr = wxcpt.VerifyURL(sVerifyMsgSig, sVerifyTimeStamp, sVerifyNonce, sVerifyEchoStr)
-        print
-        type(ret)
-        print
-        type(sEchoStr)
-
-        if (ret != 0):
-            print
-            "ERR: VerifyURL ret:" + str(ret)
-            sys.exit(1)
-        return sEchoStr
-
-    # 接收客户端消息
-    if request.method == 'POST':
-        sReqMsgSig = sVerifyMsgSig
-        sReqTimeStamp = sVerifyTimeStamp
-        sReqNonce = sVerifyNonce
-        sReqData = request.data
-        print(sReqData)
-
-        ret, sMsg = wxcpt.DecryptMsg(sReqData, sReqMsgSig, sReqTimeStamp, sReqNonce)
-        print
-        ret, sMsg
-        if (ret != 0):
-            print
-            "ERR: DecryptMsg ret: " + str(ret)
-            sys.exit(1)
-        # 解析发送的内容并打印
-
-        xml_tree = ET.fromstring(sMsg)
-        print('xml_tree is ', xml_tree)
-    return "This is for auto-reply usage.\n {}".format(msg)
+        echo_str = request.args.get('echostr', '')
+        try:
+            echo_str = crypto.check_signature(
+                signature,
+                timestamp,
+                nonce,
+                echo_str
+            )
+            print(echo_str)
+        except InvalidSignatureException:
+            abort(403)
+        return echo_str
+    else:
+        try:
+            msg = crypto.decrypt_message(
+                request.data,
+                signature,
+                timestamp,
+                nonce
+            )
+            msg = parse_message(msg)
+            if msg.type == 'text':
+                reply = create_reply(msg.content, msg).render()
+            else:
+                reply = create_reply('Can not handle this for now', msg).render()
+            res = crypto.encrypt_message(reply, nonce, timestamp)
+            return res
+        except (InvalidSignatureException, InvalidCorpIdException):
+            abort(403)
 
 
 @app.route('/')
